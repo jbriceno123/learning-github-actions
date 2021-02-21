9858805063bb476af76f8ff9c93c8604c7a8ad86
Javier Briceño <jbriceno@comparaonline.com>
2021-02-20 22:15:22 -0300

    dd

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fileChanges = require('trilom/file-changes-action@v1.2.3');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
+// const fileChanges = require('trilom/file-changes-action@v1.2.3');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context, undefined, 2)
-  console.log(`fileChanges: \n${fileChanges}`);
-  console.log(`github: \n${github}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   // const payload = JSON.stringify(github.context, undefined, 2)
+//   console.log(`fileChanges: \n${fileChanges}`);
+//   console.log(`github: \n${github}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
+
+console.log('Hola Mundo')
